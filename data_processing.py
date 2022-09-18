b8b3a8bf2bd5a11f7619d803a3538a03ac1f6736
mhilanmathew <mhilan.mathew@gmail.com>
2022-09-18 13:13:33 +0530

    added a new func sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,13 @@
+from re import A
+
+
 print('adding all the code related to fraud detection')
+
+a = 5
+b = 6
+
+def sum(a,b):
+    c= a+b
+    return a 
+
+print('written a function with wrong return value')
