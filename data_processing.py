dfdadd55b90cd254e3d5141ff14df2824b69b088
mhilanmathew <mhilan.mathew@gmail.com>
2022-09-19 23:19:37 +0530

    Revert "added a new func sum"
    This reverts commit b8b3a8bf2bd5a11f7619d803a3538a03ac1f6736.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,1 @@
-from re import A
-
-
 print('adding all the code related to fraud detection')
-
-a = 5
-b = 6
-
-def sum(a,b):
-    c= a+b
-    return a 
-
-print('written a function with wrong return value')
